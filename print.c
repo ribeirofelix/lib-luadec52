4806998537e993a3bf56be3a14eb78551d39b5c5
Felix <ribeiro.felix@live.com>
2014-05-21 14:23:00 -0300

    vetor de prioridades acertado

--- print.c
+++ print.c
@@ -1060,8 +1060,8 @@ static char *operators[23] =
    " ","+", "-", "*", "/", "%" ,"^", "-", "not ", "#" , ".."
 };
 
-static int priorities[20] =
-    { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 3, 3, 1, 2, 2, 5 };
+static int priorities[23] =
+    { 0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0, 0, 4, 4, 3, 3, 3, 1, 2, 2, 2, 5 };
 
 char *RegisterOrConstant(Function * F, int r)
 {
